85ff2a1889fedda2867eaa548198efc6e4492248
rolikun <roland.kun98@gmail.com>
2023-12-03 19:23:20 +0100

    feat(enums):add hangman phases

---
+++ src/utils/enums/hangman-phases.ts
@@ -0,0 +1,12 @@
+export enum HangmanPhases {
+  PHASE00 = 0,
+  PHASE01 = 1,
+  PHASE02 = 2,
+  PHASE03 = 3,
+  PHASE04 = 4,
+  PHASE05 = 5,
+  PHASE06 = 6,
+  PHASE07 = 7,
+  PHASE08 = 8,
+  PHASE09 = 9,
+}
