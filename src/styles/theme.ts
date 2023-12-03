d9533e46411f1273d5f1a9f32d2f187644a9a438
rolikun <roland.kun98@gmail.com>
2023-12-03 19:45:34 +0100

    feat(theme.ts):setted new theme options

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,9 +4,15 @@ import { createBreakpoints } from "@mui/system";
 const breakpoints = createBreakpoints({});
 
 export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#000",
+    },
+  },
   typography: {
     h1: {
       fontSize: "4rem",
+      fontWeight: 600,
       userSelect: "none",
       [breakpoints.down("sm")]: {
         fontSize: "2rem",
@@ -14,9 +20,16 @@ export const theme = createTheme({
     },
     h3: {
       fontSize: "2rem",
+      fontWeight: 600,
       userSelect: "none",
       [breakpoints.down("sm")]: {
         fontSize: "1.2rem",
+      },
+    },
+    subtitle1: {
+      lineHeight: "1.3",
+      [breakpoints.down("sm")]: {
+        fontSize: ".9rem",
       },
     },
   },
