368d151cd8c2cfebbd8e98b95cf816eb315f9aa4
rolikun <roland.kun98@gmail.com>
2023-12-03 19:57:51 +0100

    refactor(hangman):refactored hangman phase enum

--- src/components/elements/icons/hangman/index.tsx
+++ src/components/elements/icons/hangman/index.tsx
@@ -28,16 +28,16 @@ const HangmanIcon = ({
       }}
     >
       <svg viewBox="0 0 10 12">
-        {phase >= HangmanPhases.PHASE00 && <path d="M1,11 h8" />}
-        {phase >= HangmanPhases.PHASE01 && <path d="M9,11 v-10" />}
-        {phase >= HangmanPhases.PHASE02 && <path d="M9,1 h-4" />}
-        {phase >= HangmanPhases.PHASE03 && <path d="M5,1 v2" />}
-        {phase >= HangmanPhases.PHASE04 && <circle cx="5" cy="4" r="1" />}
-        {phase >= HangmanPhases.PHASE05 && <path d="M5,5 v3" />}
-        {phase >= HangmanPhases.PHASE06 && <path d="M5,5 l-2,2" />}
-        {phase >= HangmanPhases.PHASE07 && <path d="M5,5 l2,2" />}
-        {phase >= HangmanPhases.PHASE08 && <path d="M5,8 l-2,2" />}
-        {phase >= HangmanPhases.PHASE09 && <path d="M5,8 l2,2" />}
+        {phase >= HangmanPhases.PHASE_00 && <path d="M1,11 h8" />}
+        {phase >= HangmanPhases.PHASE_01 && <path d="M9,11 v-10" />}
+        {phase >= HangmanPhases.PHASE_02 && <path d="M9,1 h-4" />}
+        {phase >= HangmanPhases.PHASE_03 && <path d="M5,1 v2" />}
+        {phase >= HangmanPhases.PHASE_04 && <circle cx="5" cy="4" r="1" />}
+        {phase >= HangmanPhases.PHASE_05 && <path d="M5,5 v3" />}
+        {phase >= HangmanPhases.PHASE_06 && <path d="M5,5 l-2,2" />}
+        {phase >= HangmanPhases.PHASE_07 && <path d="M5,5 l2,2" />}
+        {phase >= HangmanPhases.PHASE_08 && <path d="M5,8 l-2,2" />}
+        {phase >= HangmanPhases.PHASE_09 && <path d="M5,8 l2,2" />}
       </svg>
     </SvgIcon>
   );
