5ad82007f1e5de371915b17d2d199c6ce64cddae
rolikun <roland.kun98@gmail.com>
2023-12-07 18:00:44 +0100

    feat(constants):added new constants

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -1,2 +1,8 @@
+export const GAME_TITLE = "The Hangman";
 export const RANDOM_VALUE = 0;
 export const RANDOM = "Random";
+export const SELECTED_WORD_ID = "selectedWordId";
+export const DEFAULT_MISTAKES_VALUE = 0;
+export const MISTAKES = "mistakes";
+export const GAME_STATUS = "gameStatus";
+export const USED_LETTERS = "usedLetters";
